eacd62b063b8d128690473fb52f283a45cd401d9
CZQ <chzuqi@gmail.com>
2016-06-14 17:24:00 +0800

    deep first

--- src/finders/DeepFirstFinder.js
+++ src/finders/DeepFirstFinder.js
@@ -1,28 +1,103 @@
-var AStarFinder = require('./AStarFinder');
+var Util = require('../core/Util');
+var DiagonalMovement = require('../core/DiagonalMovement');
 
 /**
- * Best-First-Search path-finder.
+ * Breadth-First-Search path finder.
  * @constructor
- * @extends AStarFinder
  * @param {Object} opt
  * @param {boolean} opt.allowDiagonal Whether diagonal movement is allowed.
  *     Deprecated, use diagonalMovement instead.
  * @param {boolean} opt.dontCrossCorners Disallow diagonal movement touching
  *     block corners. Deprecated, use diagonalMovement instead.
  * @param {DiagonalMovement} opt.diagonalMovement Allowed diagonal movement.
- * @param {function} opt.heuristic Heuristic function to estimate the distance
- *     (defaults to manhattan).
  */
 function DeepFirstFinder(opt) {
-    AStarFinder.call(this, opt);
+    opt = opt || {};
+    this.allowDiagonal = opt.allowDiagonal;
+    this.dontCrossCorners = opt.dontCrossCorners;
+    this.diagonalMovement = opt.diagonalMovement;
 
-    var orig = this.heuristic;
-    this.heuristic = function(dx, dy) {
-        return dx + dy;
-    };
+    if (!this.diagonalMovement) {
+        if (!this.allowDiagonal) {
+            this.diagonalMovement = DiagonalMovement.Never;
+        } else {
+            if (this.dontCrossCorners) {
+                this.diagonalMovement = DiagonalMovement.OnlyWhenNoObstacles;
+            } else {
+                this.diagonalMovement = DiagonalMovement.IfAtMostOneObstacle;
+            }
+        }
+    }
 }
 
-DeepFirstFinder.prototype = new AStarFinder();
-DeepFirstFinder.prototype.constructor = DeepFirstFinder;
+/**
+ * Find and return the the path.
+ * @return {Array<Array<number>>} The path, including both start and
+ *     end positions.
+ */
+DeepFirstFinder.prototype.findPath = function(startX, startY, endX, endY, grid) {
+    var openList = [],
+        diagonalMovement = this.diagonalMovement,
+        startNode = grid.getNodeAt(startX, startY),
+        endNode = grid.getNodeAt(endX, endY),
+        neighbors, neighbor, node, i, l;
+
+    // push the start pos into the queue
+    openList.push(startNode);
+    startNode.opened = true;
+
+
+    //while the queue is not empty
+    while (openList.length) {
+        // take the front node from the queue
+        node = openList.shift();
+        node.closed = true;
+
+        // reached the end position
+        if (node === endNode) {
+            return Util.backtrace(endNode);
+        }
+
+        neighbors = grid.getNeighbors(node, diagonalMovement);
+        for (i = 0, l = neighbors.length; i < l; ++i) {
+            neighbor = neighbors[i];
+
+            // skip this neighbor if it has been inspected before
+            if (neighbor.closed || neighbor.opened) {
+                continue;
+            }
+            
+            openList.unshift(neighbor);
+            neighbor.opened = true;
+            neighbor.parent = node;
+        }
+    }
+
+    //function dfs_visit(node)
+    //{
+    //    // reached the end position
+    //    if (node === endNode) {
+    //        return Util.backtrace(endNode);
+    //    }
+    //    node.opened = true;
+    //    neighbors = grid.getNeighbors(node, diagonalMovement);
+    //    for (i = 0, l = neighbors.length; i < l; ++i) {
+    //        neighbor = neighbors[i];
+    //
+    //        // skip this neighbor if it has been inspected before
+    //        if (neighbor.closed || neighbor.opened) {
+    //            continue;
+    //        }
+    //
+    //        neighbor.opened = true;
+    //        neighbor.parent = node;
+    //        dfs_visit(neighbor);
+    //    }
+    //    node.closed = true;
+    //}
+
+    // fail to find the path
+    return [];
+};
 
 module.exports = DeepFirstFinder;
